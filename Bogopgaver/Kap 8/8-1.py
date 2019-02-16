b8d17c2fbc71c6f4d1c76e72532790b522bd87bb
AnvilOfDoom <equlan@gmail.com>
2019-02-16 21:16:30 +0100

    Forberedelse til uge 5

---
+++ Bogopgaver/Kap 8/8-1.py
@@ -0,0 +1,9 @@
+"""8-1. Message
+WRite a function display_message() that prints a message when called"""
+
+#defining the function.
+def display_messagee():
+    print("Hi folks! This chapter is all about defining your own functions!")
+
+#calling the function
+display_messagee()
---
+++ Bogopgaver/Kap 8/8-2.py
@@ -0,0 +1,8 @@
+"""8-2. Favorite Book
+Define a function that accepts one parameter. The function should print a message about someone's favorite book
+with the book's title being the parameter"""
+
+def favorite_book(title):
+    print("My favorite book is " + title)
+
+favorite_book("Anabasis")
