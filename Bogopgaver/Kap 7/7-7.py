ec7318e8b8a9b33979d217c642afc0d698b35a83
AnvilOfDoom <equlan@gmail.com>
2019-02-16 12:58:32 +0100

    Forberedelse til uge 4

---
+++ Bogopgaver/Kap 7/7-7.py
@@ -0,0 +1,8 @@
+"""7-7. Infinity
+Write a loop that never ends.
+My solution to 7-5 is also a loop that never ends (if we don't count users entering non-numbers as input."""
+count = 0
+
+while True:
+    count += 1
+    print(count)
