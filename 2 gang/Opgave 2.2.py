eec3b35906c9e4039d5b450616d7611bfe9e424f
AnvilOfDoom <equlan@gmail.com>
2019-02-19 17:45:48 +0100

    Arbejde med øvelser til anden undervisningsgang

--- 2 gang/Opgave 2.2.py
+++ 2 gang/Opgave 2.2.py
@@ -4,10 +4,14 @@ yndlingsforfattere = ["Tolkien", "Pullman", "Epikur", "Mill", "Nietzsche"]
 for forfatter in yndlingsforfattere:
     print(forfatter)
 
+print("#################")
+
 #tilføjer forfatter til listen
 yndlingsforfattere.append("Hume")
 for forfatter in yndlingsforfattere:
     print(forfatter)
+
+print("##################")
 
 #fjerner nr. 2 fra listen
 
--- 2 gang/Opgave 2.5.py
+++ 2 gang/Opgave 2.5.py
@@ -1,25 +1,5 @@
 #lister med tal
 
-"""
-#Oprindeligt forsøg
-list_ten = range(1,11)
-print(type(list_ten))
-
-
-for value in list_ten:
-    print(value)
-
-#liste med 3-tabellen
-list_three = range(3,100,3)
-
-for value in list_three:
-    print(value)
-"""
-
-
-"""liste med tallene 1-10. Hvis jeg ville kunne udskrive listen som liste, hvor den faktisk giver det rigtige
-så burde jeg bruge list(range(x, x))"""
-
 list_ten = list(range(1,11))
 for value in list_ten:
     print(value)
