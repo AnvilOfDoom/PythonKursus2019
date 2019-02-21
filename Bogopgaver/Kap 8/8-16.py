44d284b1a57858d8d0f21cee04f9b4171c61d8d2
AnvilOfDoom <equlan@gmail.com>
2019-02-21 16:04:54 +0100

    Forberedelse til uge 5

---
+++ Bogopgaver/Kap 8/8-16.py
@@ -0,0 +1,15 @@
+"""8-16. Imports
+"Try out different ways of importing functions"""
+#didn't do this because og laziness and all my files have names like 8-1 and apparently those cannot be important.
+
+#import module_name
+
+
+#from module_name import function_name
+
+#from module_name import function_name as fn
+
+#import module_name as mn
+
+#from module_name import *
+
---
+++ Bogopgaver/Kap 8/printing_models.py
@@ -0,0 +1,10 @@
+import printing_functions
+
+unprinted_models = ["iphone case", "robot pendant", "dodecahedron"]
+printed_models = []
+
+printing_functions.print_models(unprinted_models, printed_models)
+
+printing_functions.show_completed_models(printed_models)
+
+
