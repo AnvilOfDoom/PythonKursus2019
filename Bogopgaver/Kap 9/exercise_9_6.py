11b9b85edabaa1b87a5c67f5d4919c4d252a36e0
AnvilOfDoom <equlan@gmail.com>
2019-02-23 11:13:38 +0100

    Forberedelse til uge 6

---
+++ Bogopgaver/Kap 9/exercise_9_6.py
@@ -0,0 +1,35 @@
+"""9-6. Ice Cream Stand
+An ice cream stand is a specific kind of restaurant.
+Write a class called IceCreamStand that inherits from the Restaurant class you wrote in
+Exercise 9-1 (page 166) or Exercise 9-4 (page 171).
+
+Either version of the class will work; just pick the one you like better.
+Add an attribute called flavors that stores a list of ice cream flavors.
+Write a method that displays theese flavors.
+Create an instance of IceCreamStand, and call this method."""
+
+#Restaurant class from 9-1:
+class Restaurant():
+    """This class is used to create restarant of a certain type."""
+
+    def __init__(self, restaurant_name, cuisine_type):
+        """Contains the name and type of the restaurant."""
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_served = 0  #added attribute for counting the number of served customers.
+
+    def describe_restaurant(self):
+        """this method describes the method, printing a message with name and the type of cuisine"""
+        print(self.restaurant_name.title() + " is a " + self.cuisine_type + " restaurant.")
+
+    def open_restaurant(self):
+        """"simulates opening the restaurant"""
+        print(self.restaurant_name.title() + " is now open!")
+
+    def set_number_served(self, customers): #new method
+        """used to directly set the number of served customers"""
+        self.number_served = customers
+
+    def increment_number_served(self, customers): #new method
+        """Add given number to number_served"""
+        self.number_served += customers
