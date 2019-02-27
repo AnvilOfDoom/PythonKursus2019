355794cb09e307cd3f4cee30a6fc32770b0965a4
AnvilOfDoom <equlan@gmail.com>
2019-02-27 19:21:46 +0100

    Forberedelse til uge 7

---
+++ Bogopgaver/Kap 10/exercise_10_13.py
@@ -0,0 +1,54 @@
+"""10-3 Verify User
+The final listing for remember_me.py assumes
+either that the user has already entered their username
+or that the program is running for the first time.
+We should modify it in case the current user is not the person who last used the program.
+
+Before printing a welcome back message in greet_user(),
+ask the user if this is the correct username.
+If it’s not, call get_new_username() to get the correct username."""
+
+#copying code from the book, my additions will be marked
+
+import json
+def get_stored_username():
+    """Get stored username if available"""
+    filename = "username.json"
+    try:
+        with open(filename) as f_obj:
+            username = json.load(f_obj)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def get_new_username():
+    """Prompt for a new username."""
+    username = input("What is your name? ")
+    filename = "username.json"
+    with open(filename, "w") as f_obj:
+        json.dump(username, f_obj)
+    return username
+
+def greet_user():
+    """Greet the user by name."""
+    username = get_stored_username()
+    if username:
+        if verify_user():
+            print("Welcome back, " + username + "!")
+        else:
+            get_new_username()
+    else:
+        username = get_new_username()
+        print("We'll remembrt you when you come back, " + username + "!")
+
+def verify_user():
+    """asks the user if the program has loaded the correct username."""
+    username = get_stored_username()
+    verified = input("Is " + username + " your username? If yes type y, if no type n: ")
+    if verified == "y":
+        return True
+    else:
+        return None
+
+greet_user()
