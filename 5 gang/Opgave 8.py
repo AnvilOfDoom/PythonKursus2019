e3bc63fd13f2666f65ce1d2ebe6bf070ffff186c
AnvilOfDoom <equlan@gmail.com>
2019-03-05 20:41:43 +0100

    5. undervisningsgang

--- 5 gang/Opgave 8.py
+++ 5 gang/Opgave 8.py
@@ -7,3 +7,18 @@ print("Resultatet er " + kvadrat(5))
 print("")
 print("#########################")
 print("")
+
+from typing import List
+
+def minsum(liste : List[int]) -> str:
+    sum_af_liste = 0
+    for x in liste:
+        sum_af_liste += x
+    return str(sum_af_liste)
+
+talliste = [1, 2, 3, 4]
+print("Summen af min liste er " + minsum(talliste))
+
+###
+liste2 = ["anders","martin","benny"]
+print("summen er : "+minsum(liste2)) #denne markeres med en advarsel fordi funktionen forventer en liste af integers.
